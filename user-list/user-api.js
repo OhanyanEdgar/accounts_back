f0ad68315dc8024bb2f8568232197e8f23931218
Edgar Ohanyan <edgarohanyan1994@gmail.com>
2022-06-27 19:37:39 +0400

    refresh issue

--- user-list/user-api.js
+++ user-list/user-api.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -8,35 +8,35 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-let users = []
+let users = [];
 
 app.get('/', (req, res) => {
-    res.json(users)
-})
+    res.json(users);
+});
 
 app.get('/user/:id', (req, res) => {
     const id = req.params.id;
-    res.json(users.find(user => user.id === id));
+    const userIndex = users.findIndex(user => user.id === id);
+    if (userIndex !== -1) res.json(users[userIndex]);
 });
 
 app.post('/add_user', (req, res) => {
     const user = req.body.user;
-    res.json(users.push(user));
+    if (user) users.push(user);
+    res.status(201).send('User created.');
 });
 
 app.post('/edit_user', (req, res) => {
     const reqUser = req.body.user;
-    users = [...users.map(user => {
-        if (user.id == reqUser.id) return reqUser;
-        return user
-    })];
+    const tempUserIndex = users.findIndex(user => user.id === reqUser.id);
+    if (tempUserIndex !== -1) users[tempUserIndex] = reqUser;
+
     res.json(users);
 });
 
 app.delete('/user/:id', (req, res) => {
     const id = req.params.id;
-    users = users.filter(user => user.id !== id);
     res.json(users.filter(user => user.id !== id));
-})
+});
 
 app.listen(port, () => console.log(`User CRUD app listening on port ${port}!`));
