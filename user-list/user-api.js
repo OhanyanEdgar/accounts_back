decac5fb4fc75d9f03a4ceb7c98d645b688bf138
Edgar Ohanyan <edgarohanyan1994@gmail.com>
2022-06-27 12:43:06 +0400

    added add delete and edit functionalities to crud

--- user-list/user-api.js
+++ user-list/user-api.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const users = require('./dummy_data')
 
 const app = express();
 const port = 5000;
@@ -9,16 +8,35 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+let users = []
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.json(users)
 })
 
 app.get('/user/:id', (req, res) => {
     const id = req.params.id;
-
     res.json(users.find(user => user.id === id));
-    res.status(404).send('User not found');
 });
+
+app.post('/add_user', (req, res) => {
+    const user = req.body.user;
+    res.json(users.push(user));
+});
+
+app.post('/edit_user', (req, res) => {
+    const reqUser = req.body.user;
+    users = [...users.map(user => {
+        if (user.id == reqUser.id) return reqUser;
+        return user
+    })];
+    res.json(users);
+});
+
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    users = users.filter(user => user.id !== id);
+    res.json(users.filter(user => user.id !== id));
+})
 
 app.listen(port, () => console.log(`User CRUD app listening on port ${port}!`));
